f98e5841d5f1a966d181fdc9d20ffd854765d74e
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-07-03 23:03:45 +0300

    added connection to the database file

---
+++ server/db.js
@@ -0,0 +1,14 @@
+import pg from "pg";
+const { Pool } = pg;
+import dotenv from "dotenv";
+dotenv.config();
+
+export const opendB = () => {
+  return new Pool({
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+};
