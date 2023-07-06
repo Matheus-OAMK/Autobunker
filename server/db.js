2d4f835d8ce46ea8b5c7680dfb9d7db34380e75b
Matheus-OAMK <t2fema00@students.oamk.fi>
2023-07-06 16:47:06 +0300

    Added ssl settings for remote case in the db connection

--- server/db.js
+++ server/db.js
@@ -1,14 +1,33 @@
 import pg from "pg";
 const { Pool } = pg;
+import fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const opendB = () => {
-  return new Pool({
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-  });
+  switch (process.env.DB_LOCATION) {
+    case "local":
+      return new Pool({
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      });
+
+    case "remote":
+      return new Pool({
+        databse: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        ssl: {
+          rejectUnauthorized: false,
+          ca: fs.readFileSync(`${__dirname}/DB_SSL/root.crt`).toString(),
+          key: fs.readFileSync(`${__dirname}/DB_SSL/server.key`).toString(),
+          cert: fs.readFileSync(`${__dirname}/DB_SSL/server.crt`).toString(),
+        },
+      });
+  }
 };
